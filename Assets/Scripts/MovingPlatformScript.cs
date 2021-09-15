b80f46a87fea7f651dc06e62c1321c9344d48940
Diego Doná <diedona@gmail.com>
2021-09-15 14:56:39 -0300

    moving platform

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -5,18 +5,48 @@ using UnityEngine;
 
 public class MovingPlatformScript : MonoBehaviour
 {
+    [SerializeField]
+    private float _DistanceForward = 5;
 
     [SerializeField]
     private float _Speed = 1;
 
+    private float _StartingX;
+
     private void Awake()
     {
-        
+        _StartingX = transform.position.x;
     }
 
     private void Update()
     {
+        //transform.position = new Vector2(transform.position.x + _Speed * Time.deltaTime, transform.position.y);
+        Debug.Log($"currentPosition.x: {transform.position.x}");
+
+        if(!IsAtDestiny())
+            MoveToDestiny();        
+        else
+            RotateDirection();
+    }
+
+    private bool IsAtDestiny()
+    {
+        if(_Speed > 0)
+            return (transform.position.x >= _StartingX + _DistanceForward);
+        else
+            return (transform.position.x <= _StartingX + _DistanceForward);
+    }
+
+    private void MoveToDestiny()
+    {
         transform.position = new Vector2(transform.position.x + _Speed * Time.deltaTime, transform.position.y);
+    }
+
+    private void RotateDirection()
+    {
+        _StartingX = transform.position.x;
+        _DistanceForward = (-_DistanceForward);
+        _Speed = (-_Speed);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
